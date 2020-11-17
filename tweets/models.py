8e8aae98e6cdf51831441754db14be43892e9683
feezghost <feezoocrazy420@gmail.com>
2020-11-17 21:24:59 +0500

    added app tweets

---
+++ tweets/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Tweets(models.Model):
+    content = models.TextField(blank=True, null=True)
+    image = models.FileField(upload_to='images/', blank=True, null=True)
+
+
+    def __str__(self):
+        return self.content
+
